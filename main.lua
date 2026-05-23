4cf2f781b13a89bad8f38a0071704f22a23521dd
nexus113fdfcs-beep <nexus113fdfcs@gmail.com>
2026-05-23 11:46:11 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
--- Der Loader, den deine Nutzer ausführen:
-local Player = game:GetService("Players").LocalPlayer
 local ScriptURL = "https://raw.githubusercontent.com/nexus113fdfcs-beep/Nexus/refs/heads/main/nexus.lua"
+
+loadstring(game:HttpGet(ScriptURL))()
