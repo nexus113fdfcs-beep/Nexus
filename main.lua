0db43d593df7109ad00a3e94cfd1ac614fabc9b9
nexus113fdfcs-beep <nexus113fdfcs@gmail.com>
2026-05-23 14:48:06 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-local ScriptURL = "https://raw.githubusercontent.com/nexus113fdfcs-beep/Nexus/main/nexus.lua"
+local ScriptURL = "https://raw.githubusercontent.com/nexus113fdfcs-beep/Nexus/refs/heads/main/nexus.lua"
 
 loadstring(game:HttpGet(ScriptURL))()
